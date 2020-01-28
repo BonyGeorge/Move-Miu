1144464f648d394c80da984997f381f98cf3a79d
Islam Magdy <DashaDeshozz@gmail.com>
2020-01-28 17:24:53 +0200

    small fix for login path

--- Login & Register/login1.php
+++ Login & Register/login1.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('Database.php');
+include('../DataBase/Database.php');
 $DB = new Database();
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -34,7 +34,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                 setcookie("password", "");
             }
         }
-        header('Location:./index.php');   
+        header('Location:../index.php');   
 
     }
 }
