22742693ec9b581ed951d51b6ba35a1d9459908a
Islam Magdy <DashaDeshozz@gmail.com>
2020-02-04 18:21:49 +0200

    some chnages

--- Login & Register/login.php
+++ Login & Register/login.php
@@ -1,214 +1,228 @@
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login & Register</title>
-        <script src="../js/Register_validation.js"> </script>
-        <script src="../js/jquery-3.3.1.min.js" ></script>
-        <link rel="stylesheet" href="../css/style.css">
-        <link rel="icon" sizes="128x128" href="../images/fav.png">
-        <style>
-        /* Regiseer */
-        #font{  
-color: Green;
-font-family: Arial, Helvetica, sans-serif;
-font-size: 150%;
-font: italic  27px/30px;
-top:-23px;
 
-}
-h1 {
-            text-shadow: 2px 2px #7CFC00;
-            color: Green;
-            font-family: verdana;
-            font-size: 300%;
-             height:100%;
-          
-            font: italic bold 40px/30px Georgia, serif;
-          
-          
-          }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login & Register</title>
+    <script src="../js/Register_validation.js"> </script>
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" sizes="128x128" href="../images/fav.png">
+    <style>
+    /* Regiseer */
+    #font {
+        color: Green;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 150%;
+        font: italic 27px/30px;
+        top: -23px;
 
-        #UserNamee {
-  border-radius: 25px;
-  border: 2px solid #73AD21;
-  padding: 20px; 
-  width: 100%;
-  height: 50px;  
-  
-}
-#fullname {
-  border-radius: 25px;
-  border: 2px solid #73AD21;
-  padding: 20px; 
-  width: 100%;
-  height: 50px;  
-  
-}
-#Email {
-  border-radius: 25px;
-  border: 2px solid #73AD21;
-  padding: 20px; 
-  width: 100%;
-  height: 20px;  
+    }
 
-  
-}
-#id {
-  border-radius: 25px;
-  border: 2px solid #73AD21;
-  padding: 15px; 
-  width:100%;
-  height: 20px;  
-  
-}
-#passReg {
-  border-radius: 20px;
-  border: 2px solid #73AD21;
-  padding: 10px; 
-  width: 100%;
-  height: 50px;  
+    h1 {
+        text-shadow: 2px 2px #7CFC00;
+        color: Green;
+        font-family: verdana;
+        font-size: 300%;
+        height: 100%;
 
-  
-}
-
-/* Login css */
-#rcorners2 {
-  border-radius: 25px;
-  border: 2px solid #73AD21;
-  padding: 20px; 
-  width: 100%;
-  height: 50px;  
-  
-}
-#user{
-
-  color: Green;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 200%;
-  font: italic  27px/30px;
-  top:-23px;
-  
-}
-#pass{
-
-color: Green;
-font-family: Arial, Helvetica, sans-serif;
-font-size: 200%;
-font: italic  27px/30px;
-top:-23px;
-
-}
-
-h1 {
-  text-shadow: 2px 2px #7CFC00;
-  color: Green;
-  font-family: verdana;
-  font-size: 300%;
-   height:100%;
-
-  font: italic bold 40px/30px Georgia, serif;
+        font: italic bold 40px/30px Georgia, serif;
 
 
-}
+    }
 
-#remember_me {
-width: 45px;
-height: 15px;
-background: #555;
-margin: 20px 80px;
-left:200px;
-top:0px;
+    #UserNamee {
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 20px;
+        width: 100%;
+        height: 50px;
 
-position: relative;
-border-radius: 5px;
-}
-#remember {
+    }
 
-  color: Green;
-  font-family: verdana;
-  font-size: 300%;
-  font: italic bold 12px/30px Georgia, serif;
-  text-alignment:center;
-  margin-left:-100px;
-}
-#image {
+    #fullname {
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 20px;
+        width: 100%;
+        height: 50px;
 
-/* margin-bottom:-20%; */
-margin-top:10%;
-margin-left:200px;
-height: 620px;
-width: 50%;
+    }
 
-}
-#square {
-
-    margin-top:-50%;
-    margin-left:1070px;
-    height: 622px;
-  width: 30%;
-
-}
-
-#buttonLog {
-
-margin-top:-10%;
-margin-left:100px;
-height: 10%;
-width: 300px;
-
-}
-#forgetPass{
-    margin-top:-50px;
-
-    font-size: 20px;
+    #Email {
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 20px;
+        width: 100%;
+        height: 20px;
 
 
-height: 50px;
-width: 100px;
-}
+    }
 
-</style>
-    </head>
-    
-    <body>
-    <img src="Move.jpeg"  id="image"> 
+    #id {
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 15px;
+        width: 100%;
+        height: 20px;
+
+    }
+
+    #passReg {
+        border-radius: 20px;
+        border: 2px solid #73AD21;
+        padding: 10px;
+        width: 100%;
+        height: 50px;
+
+
+    }
+
+    /* Login css */
+    #rcorners2 {
+        border-radius: 25px;
+        border: 2px solid #73AD21;
+        padding: 20px;
+        width: 100%;
+        height: 50px;
+
+    }
+
+    #user {
+
+        color: Green;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 200%;
+        font: italic 27px/30px;
+        top: -23px;
+
+    }
+
+    #pass {
+
+        color: Green;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 200%;
+        font: italic 27px/30px;
+        top: -23px;
+
+    }
+
+    h1 {
+        text-shadow: 2px 2px #7CFC00;
+        color: Green;
+        font-family: verdana;
+        font-size: 300%;
+        height: 100%;
+
+        font: italic bold 40px/30px Georgia, serif;
+
+
+    }
+
+    #remember_me {
+        width: 45px;
+        height: 15px;
+        background: #555;
+        margin: 20px 80px;
+        left: 200px;
+        top: 0px;
+
+        position: relative;
+        border-radius: 5px;
+    }
+
+    #remember {
+
+        color: Green;
+        font-family: verdana;
+        font-size: 300%;
+        font: italic bold 12px/30px Georgia, serif;
+        text-alignment: center;
+        margin-left: -100px;
+    }
+
+    #image {
+
+        /* margin-bottom:-20%; */
+        margin-top: 10%;
+        margin-left: 200px;
+        height: 620px;
+        width: 50%;
+
+    }
+
+    #square {
+
+        margin-top: -50%;
+        margin-left: 1070px;
+        height: 622px;
+        width: 30%;
+
+    }
+
+    #buttonLog {
+
+        margin-top: -10%;
+        margin-left: 100px;
+        height: 10%;
+        width: 300px;
+
+    }
+
+    #forgetPass {
+        margin-top: -50px;
+
+        font-size: 20px;
+
+
+        height: 50px;
+        width: 100px;
+    }
+    </style>
+</head>
+
+<body>
+    <img src="Move.jpeg" id="image">
     <?php include("../Template/header.php");?>
 
-        <div class="form" id="square" style="margin-left:1000px;bottom:50px;">
-       
+    <div class="form" id="square" style="margin-left:1000px;bottom:50px;">
 
-            <ul class="tab-group">
-                <li class="tab  "><a href="#signup">Register</a></li>
-                <li class="tab active"><a href="#login">Log In</a></li>
-            </ul>
-            <div class="tab-content">
-                <div id="login">   
-                    <h1>Welcome Movers!</h1>
-                    <span style="color:red; font-weight:bold; position:relative; bottom:15px;" ><?php if( !empty( $_REQUEST['Message'] )){echo $_REQUEST['Message'];} ?></span>
-                    <form action="login1.php" method="post">
-                        <div class="field-wrap">
-                            <label id="user">
-                                User Name:<span  class="req"></span>
-                            </label>
-                            </br>
-                            <input name="Username" id="rcorners2"  type="text"required autocomplete="off"/>
-                        </div>
 
-                        <div class="field-wrap">
-                            <label id="pass">
-                                Password:<span class="req"></span>
-                            </label>
-                            </br>
-                            <input name="Password" id="rcorners2" type="password"required autocomplete="off"/>
-                        </div>
-                        <div class="field-wrap">
-                            <input type="checkbox"  name="remember_me" id="remember_me"><label id="remember" style="top:-15px;left:230px;font-size: 20px;">Remember Me</label>
-                        </div>
-                        <a id="forgetPass" href="#" style="margin-left:23%;">Forget Your Password?!</a><br><br><br><br><br>
-                        <button class="button button-block" id="buttonLog   ">Log In</button>
-                    </form>
+        <ul class="tab-group">
+            <li class="tab  "><a href="#signup">Register</a></li>
+            <li class="tab active"><a href="#login">Log In</a></li>
+        </ul>
+        <div class="tab-content">
+            <div id="login">
+                <h1>Welcome Movers!</h1>
+                <span
+                    style="color:red; font-weight:bold; position:relative; bottom:15px;"><?php if( !empty( $_REQUEST['Message'] )){echo $_REQUEST['Message'];} ?></span>
+                <form action="login1.php" method="post">
+                    <div class="field-wrap">
+                        <label id="user">
+                            User Name:<span class="req"></span>
+                        </label>
+                        </br>
+                        <input name="Username" id="rcorners2" type="text" required autocomplete="off" />
+                    </div>
+
+                    <div class="field-wrap">
+                        <label id="pass">
+                            Password:<span class="req"></span>
+                        </label>
+                        </br>
+                        <input name="Password" id="rcorners2" type="password" required autocomplete="off" />
+                    </div>
+                    <div class="field-wrap">
+                        <input type="checkbox" name="remember_me" id="remember_me"><label id="remember"
+                            style="top:-15px;left:230px;font-size: 20px;">Remember Me</label>
+                    </div>
+                    <a id="forgetPass" href="#" onclick="document.getElementById('smail').style.display='block'"
+                        style="margin-left:23%;">Forget Your Password?!</a><br><br><br><br><br>
+                    <button class="button button-block" id="buttonLog   ">Log In</button>
+                </form>
             </div>
             <div id="signup">
                 <h1>Join Us! </h1>
@@ -228,7 +242,7 @@ width: 100px;
 
                         <div class="field-wrap">
                             <label id="font" style="margin-top:-3%;">
-                                User Name:<span class="req" >*</span>
+                                User Name:<span class="req">*</span>
                             </label>
                             <input onblur="checkuserr()" id="UserNamee" name="username" type="text" required
                                 autocomplete="off" />
@@ -240,7 +254,8 @@ width: 100px;
                         <label id="font" style="margin-top:-7%;">
                             Email:<span class="req">*</span>
                         </label>
-                        <input onblur="checkmail()" id="Email" name="Email" type="email"  style="margin-top:-70px;" required autocomplete="off" />
+                        <input onblur="checkmail()" id="Email" name="Email" type="email" style="margin-top:-70px;"
+                            required autocomplete="off" />
                         <span style=" color:red; display:block; margin-bottom:20px;" id="mail"></span>
 
                     </div>
@@ -249,7 +264,8 @@ width: 100px;
                         <label id="font" style="margin-top:-5%;">
                             University ID:<span class="req">*</span>
                         </label>
-                        <input onblur="validateUniID(this)" name="id" type="text" required autocomplete="off" id="id" maxlength="9">
+                        <input onblur="validateUniID(this)" name="id" type="text" required autocomplete="off" id="id"
+                            maxlength="9">
                         <span style="color:red; display:block; margin-bottom:20px;" id="uniID"></span>
 
                     </div>
@@ -262,15 +278,46 @@ width: 100px;
                             autocomplete="off">
                         <span style="color:red; display:block; margin-bottom:20px;"></span>
                     </div>
-                    <button type="submit" onclick="return check();" class="button button-block" id="buttonLog" style="margin-left:70px;">Register</button>
+                    <button type="submit" onclick="return check();" class="button button-block" id="buttonLog"
+                        style="margin-left:70px;">Register</button>
                 </form>
-                </div>
-                </div>                
-        </div><!-- tab-content -->
+            </div>
+        </div>
+    </div><!-- tab-content -->
     </div> <!-- /form -->
     <br><br><br>
     <script src="../js/script.js"></script>
     <script type="text/javascript" src="../js/slider.js"></script>
- </body>
+    <div id="smail" class="modal">
+        <form class="modal-content animate" method="POST" action="changeToAdmin.php">
+            <div class="imgcontainer">
+                <script>
+                var modal = document.getElementById('smail');
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+                </script>
+                <span onclick="document.getElementById('smail').style.display='none'" class="close"
+                    title="Close Modal">&times;</span>
+            </div>
+
+            <div class="container">
+                <center>
+                    <h1>
+                        <h3>Send change password mail</h3>
+                    </h1>
+                </center>
+                <input id="username" type="text" placeholder="send mail" name="username" required>
+                <center><input type='submit' id='btn' name='submitAdmin' class='btn btn-appoint' value='ADD'></center>
+            </div>
+        </form>
+    </div>
+    <script src="../js/script.js"></script>
+    <script type="text/javascript" src="../js/slider.js"></script>
+</body>
+
 </html>
 <?php include('../Template/footer.html');?>
---
+++ Operations/sendpwmail.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+include '../DataBase/Database.php';
+$DB = new Database();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception; 
+
+require '../composer/vendor/autoload.php'; 
+
+date_default_timezone_set('Etc/UTC'); 
+
+$mail = new PHPMailer(TRUE);
+$mail->SMTPOptions = array('ssl'=>array('verify_peer'=>false, 'verify_peer_name'=>false, 'allow_self_signed'=>true));
+$mail->isSMTP(); 
+$mail->Host = 'smtp.gmail.com'; 
+$mail->Port = 587; 
+$mail->SMTPAuth = true; 
+$mail->SMTPSecure = 'tls'; 
+$mail->Username = 'move20miu2020@gmail.com'; 
+$mail->Password = 'rywuqlxruswomhuj'; 
+$mail->setFrom('move20miu2020@gmail.com', $_SESSION['name']. ' from Move'); 
+$mail->CharSet = 'utf-8';
+$mail->isHTML(true);
+
+if (isset($_POST['send1'])){
+    $rid = $_POST['selection'];
+    $sql = "SELECT * FROM users where id = '$rid'";
+    $DB->query($sql);
+    $DB->execute();
+    $x=$DB->getdata();
+    $uname = $x[0]->username;
+    $email = $x[0]->email;
+    $mailsubject = $_POST['mailsubject'];
+    $mailcontent = $_POST['mailcontent'];
+    $mail->Subject = $mailsubject;
+    $email_vars = array(
+     'name' => $uname,
+     'content' => $mailcontent,
+    );
+    $body = file_get_contents('htmlemail.html');
+    if(isset($email_vars)){
+      foreach($email_vars as $k=>$v){
+        $body = str_replace('{'.strtoupper($k).'}', $v, $body);
+      }
+    }
+    $mail->MsgHTML($body);
+    $mail->addAddress("$email", "$uname");
+    $mail->send();   
+}
+if (isset($_POST['send0'])){
+    $sql = "SELECT * FROM users";
+    $DB->query($sql);
+    $DB->execute();
+    $x=$DB->getdata();
+    for ($i=0 ; $i<$DB->numRows(); $i++){
+        $uname = $x[$i]->username;
+        $email = $x[$i]->email;
+        $mailsubject = filtervar($_POST['mailsubject'], FILTER_SANITIZE_STRING);
+        $mailcontent = filtervar($_POST['mailcontent'], FILTER_SANITIZE_STRING);
+        $mail->Subject = $mailsubject;
+        $email_vars = array(
+         'name' => $uname,
+         'content' => $mailcontent,
+        );
+        $body = file_get_contents('htmlemail.html');
+        if(isset($email_vars)){
+          foreach($email_vars as $k=>$v){
+            $body = str_replace('{'.strtoupper($k).'}', $v, $body);
+          }
+        }
+        $mail->MsgHTML($body);
+        $mail->addAddress("$email", "$uname");
+        $mail->send();
+
+    }
+}
+header('location:sendmail.php');
+?>
