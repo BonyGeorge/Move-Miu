764e2e72188548f427e1a4858235fece702ebb1a
Islam Magdy <DashaDeshozz@gmail.com>
2020-02-12 17:37:45 +0200

    signup fix

--- Login & Register/signup.php
+++ Login & Register/signup.php
@@ -3,18 +3,18 @@ session_start();
 include('../DataBase/Database.php');
 $DB = new Database();
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
+if(isset($_POST['sumb'])){
     $name = filter_var($_POST["fullname"], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST["Email"], FILTER_SANITIZE_EMAIL);
     $username= filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $id= filter_var($_POST["id"], FILTER_SANITIZE_INT);
+    $id= $_POST["id"];
     $password= filter_var($_POST["password"], FILTER_SANITIZE_STRING);
       try{
             $sql="insert into users(name,email,universityid,username,password,type) values('".$name."' ,  '".$email."' ,  '".$id."' , '".$username."' , '".sha1($_POST["password"])."','user' )";
             $DB->query($sql);
             $DB->execute();
         
-      }catch (Exception $e) {
+      }catch (PDOException $e) {
             header('Location:login.php');
         }
         header('Location:login.php');
