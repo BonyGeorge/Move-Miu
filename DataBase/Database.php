1ba9c7af4a99ab125ffcb425fa2a691bd69f8492
fadybassel1 <56454311+fadybassel1@users.noreply.github.com>
2020-06-18 19:36:58 +0200

    Update Database.php

--- DataBase/Database.php
+++ DataBase/Database.php
@@ -2,10 +2,10 @@
 
 class Database
 {
-    private $host = 'server116.web-hosting.com';
-    private $user = 'movegvka_admin';
-    private $pass = 'Admin2020';
-    private $dbName = 'movegvka_move';
+    private $host = 
+    private $user = 
+    private $pass = 
+    private $dbName =
 
     private $dbh;
     private $stmt;
